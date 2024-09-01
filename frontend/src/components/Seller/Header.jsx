a1e682957fca57ab56fc5abf89aba3ef2e89bf67
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-01 17:23:33 +0530

    change header size

--- frontend/src/components/Seller/Header.jsx
+++ frontend/src/components/Seller/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <header className="py-2 px-3 lg:px-4 top-0 sticky text-violet-600 border-b border-gray-200 bg-white lg:hidden">
-            <div className="container flex justify-between h-[75px]">
+            <div className="container flex justify-between h-[55px]">
                 <a rel="noopener noreferrer" href="dashboard" aria-label="Back to homepage" className="flex items-center p-2">
                     <h2 className='text-xl font-bold'>Mini Store</h2>
                 </a>
