bc800823841271b63de5f37591d6a66428fa4d65
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-04 23:22:56 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors";
 const app = express()
 
-app.use(cors({
-    origin: 'https://www.veelo.in' // Allow only your frontend origin
-    // origin: '*' // Allow any origin (less secure, use with caution)
-}));
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
