268e997f5a79b6a2d527ffbec3064a60824a3b84
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-05 21:56:28 +0530

    updated multer file

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -4,7 +4,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/tmp")
+        cb(null, "/tmp")
     },
     filename: function (req, file, cb) {
         crypto.randomBytes(12, function (err, bytes) {
