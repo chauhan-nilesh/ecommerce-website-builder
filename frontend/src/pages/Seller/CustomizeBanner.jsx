77e4b652d1ebae1071661dab4d64d8c42cbe8a43
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-08 12:11:49 +0530

    updated image upload loading effect

--- frontend/src/pages/Seller/CustomizeBanner.jsx
+++ frontend/src/pages/Seller/CustomizeBanner.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import useStoreData from '../../Hooks/useStoreData'
+import React, { useState } from 'react';
+import useStoreData from '../../Hooks/useStoreData';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
