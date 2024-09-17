9bc5086bc560dae6761c7c713f6c6ea328bc9b77
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-17 11:40:08 +0530

    aade some more things in order page

--- frontend/src/pages/Seller/SellerOrderPage.jsx
+++ frontend/src/pages/Seller/SellerOrderPage.jsx
@@ -147,7 +147,7 @@ function SellerOrderPage() {
                             value={status}
                             className="w-full rounded-md bg-gray-100 border border-gray-400 px-4 py-3 text-baseoutline-none"
                         >
-                            <option value="">Pending</option>
+                            <option value="" disabled>Pending</option>
                             <option value="rejected">Rejected</option>
                             <option value="accepted">Accepted</option>
                             <option value="processed">Processed</option>
