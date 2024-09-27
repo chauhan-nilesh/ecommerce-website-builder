54c1bf6a087143632036355af04bc23262703c75
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-27 19:55:31 +0530

    updated database

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const dbConnect = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/dukaan`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/eazzy`)
         console.log("MONGODB Connected Successfully. DB HOST:", connectionInstance.connection.host)
     } catch (error) {
         console.log("MONGODB Connection Failed. Error: ", error)
