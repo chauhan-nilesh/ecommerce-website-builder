debc95f98267c3949eab80b3f5a5594cb0ca53c3
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-09-29 00:26:17 +0530

    updated pricing page

--- frontend/src/components/Home/Pricing.jsx
+++ frontend/src/components/Home/Pricing.jsx
@@ -13,22 +13,46 @@ function Pricing() {
                     <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
                         <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow-lg sm:p-8 bg-gray-50">
                             <div className="space-y-2">
-                                <h4 className="text-2xl font-bold">Beginner</h4>
-                                <span className="text-6xl font-bold">Free</span>
+                                <h4 className="text-2xl font-bold">Startup</h4>
+                                <h2 className="text-6xl font-bold">₹0 <span className='text-3xl text-gray-600 line-through'>₹999</span></h2>
                             </div>
-                            <p className="mt-3 leading-relaxed text-gray-600">Perfect for getting started</p>
+                            <p className="mt-3 leading-relaxed text-gray-600">Perfect for creators,startup and influencers</p>
                             <ul className="flex-1 mb-6 text-gray-600">
                                 <li className="flex mb-2 space-x-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
                                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                                     </svg>
-                                    <span>1 Online Store</span>
+                                    <span>Online Store</span>
                                 </li>
                                 <li className="flex mb-2 space-x-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
                                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                                     </svg>
-                                    <span>1 Staff Account</span>
+                                    <span>Own subdomain</span>
+                                </li>
+                                <li className="flex mb-2 space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
+                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                    </svg>
+                                    <span>Unlimited products</span>
+                                </li>
+                                <li className="flex mb-2 space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
+                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                    </svg>
+                                    <span>Unlimited orders & sales</span>
+                                </li>
+                                <li className="flex mb-2 space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
+                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                    </svg>
+                                    <span>Create unlimited coupons</span>
+                                </li>
+                                <li className="flex mb-2 space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
+                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                    </svg>
+                                    <span>No transaction fees</span>
                                 </li>
                                 <li className="flex mb-2 space-x-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-orange-600">
