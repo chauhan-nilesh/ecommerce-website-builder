75c628d7289e0694633b6328bad1c1374cdda9b5
chauhan-nilesh <nileshchauhan3998@gmail.com>
2024-10-05 10:49:56 +0530

    chnage button color

--- frontend/src/pages/Seller/Orders.jsx
+++ frontend/src/pages/Seller/Orders.jsx
@@ -349,7 +349,7 @@ function Orders() {
                     </button>
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
                       onClick={(e) => acceptOrderStatus(orderStatusId)}
                     >
                       Accept order
