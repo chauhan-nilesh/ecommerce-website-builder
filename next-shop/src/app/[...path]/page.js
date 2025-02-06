70c9a39b610bff5e0bcc0d251a75534d2eb53174
chauhan-nilesh <nileshchauhan3998@gmail.com>
2025-02-06 18:39:10 +0530

    updated files

--- next-shop/src/app/[...path]/page.js
+++ next-shop/src/app/[...path]/page.js
@@ -1,9 +1,9 @@
-"use client";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+export const metadata = {
+  title: "My SSR Page",
+  description: "This is the server-side rendered homepage.",
+};
 
 export default function ViteApp() {
-  const pathname = usePathname();
 
   useEffect(() => {
     // Redirect to the Vite app and preserve the path
