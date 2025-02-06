f13394617ce988ce580a1a9c69b52f85b928928b
chauhan-nilesh <nileshchauhan3998@gmail.com>
2025-02-06 21:47:42 +0530

    updated font size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,10 +11,10 @@ export default {
         sans: ['Poppins', 'sans-serif'], // Set Poppins as the default sans font
       },
       fontSize: {
-        base: '0.875rem', // 14px
-        sm: '0.75rem',    // 12px
-        lg: '1rem',       // 16px
-        xl: '1.125rem',   // 18px
+        // base: '0.875rem', // 14px
+        // sm: '0.75rem',    // 12px
+        // lg: '1rem',       // 16px
+        // xl: '1.125rem',   // 18px
         '2xl': '1.25rem', // 20px
         '3xl': '1.5rem',  // 24px
         '4xl': '1.875rem',// 30px
