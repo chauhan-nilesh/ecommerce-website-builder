a382e29faf20c733ea1d230bc39619ebff70b5a6
chauhan-nilesh <nileshchauhan3998@gmail.com>
2025-02-06 21:43:58 +0530

    Updated Shop

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/shop/", // Ensures correct asset loading
   plugins: [react()],
 });
